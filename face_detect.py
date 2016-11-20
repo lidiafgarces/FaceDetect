0f9e5c2b5b829f5edae08acb80d787d34407388d
Lidia <lidia.f.garces@gmail.com>
2016-11-20 21:34:25 +0100

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -3,7 +3,7 @@ import sys
 
 # Get user supplied values
 imagePath = sys.argv[1]
-cascPath = haarcascade_frontalface_default.xml
+cascPath = "haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
